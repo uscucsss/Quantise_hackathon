8dc7c05eb0d78dddeed4473c3e9bde6dbdc89da3
FurryFemboy4iK <kirillguskow@yandex.ru>
2026-09-18 12:49:05 +0300

    iogin page

--- frontend/src/js/home/index.js
+++ frontend/src/js/home/index.js
@@ -1,1 +1,12 @@
-console.log("Confession JS успешно подключен!");
+const btnLogin = document.getElementById('btn-login');
+const btnRegister = document.getElementById('btn-register');
+
+// Перенаправление на страницу авторизации, сразу на форму Входа
+btnLogin.addEventListener('click', () => {
+  window.location.href = 'pages/login.html#login';
+});
+
+// Перенаправление на страницу авторизации, сразу на форму Регистрации
+btnRegister.addEventListener('click', () => {
+  window.location.href = 'pages/login.html#register';
+});
